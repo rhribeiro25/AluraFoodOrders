2d18cede2a6dc330ad14894f0b83f8ec28d5a4cc
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-08-25 17:58:28 +0100

    changing database from mysql to postgres

--- src/main/resources/db/migration/V1__create_order_table.sql
+++ src/main/resources/db/migration/V1__create_order_table.sql
@@ -1,5 +1,5 @@
-CREATE TABLE orders (
-  id bigint CONSTRAINT pk_id_order PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS orders (
+  id bigserial CONSTRAINT pk_id_order PRIMARY KEY,
   order_date date NOT NULL,
   order_status varchar(255) NOT NULL
 );
--- src/main/resources/db/migration/V2__create_order_items_table.sql
+++ src/main/resources/db/migration/V2__create_order_items_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS order_items (
-  id bigint CONSTRAINT pk_id_order_item PRIMARY KEY,
+  id bigserial CONSTRAINT pk_id_order_item PRIMARY KEY,
   description varchar(255) DEFAULT NULL,
   qtt integer NOT NULL,
   order_id bigint NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES orders(id)
-);
+  FOREIGN KEY (fk_order_id) REFERENCES orders(id)
+)
