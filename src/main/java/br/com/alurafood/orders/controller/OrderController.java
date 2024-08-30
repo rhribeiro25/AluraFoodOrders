afb27b6c356b574f5941d637357dd55f5d46a81b
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-08-30 02:51:48 +0100

    Setting fanout exchange and direct exchange

--- src/main/java/br/com/alurafood/orders/controller/OrderController.java
+++ src/main/java/br/com/alurafood/orders/controller/OrderController.java
@@ -7,9 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
--- src/main/java/br/com/alurafood/orders/dto/OrderDto.java
+++ src/main/java/br/com/alurafood/orders/dto/OrderDto.java
@@ -3,7 +3,6 @@ package br.com.alurafood.orders.dto;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
